bf51a72f7fe9a917229781a5856a9e8bb572e6b5
Paritosh Batish <batishparitosh2@gmail.com>
2024-01-01 15:45:55 +0530

    updated 01-01-24 solution

--- daily-ds/2024/Jan/01-01-24.cpp
+++ daily-ds/2024/Jan/01-01-24.cpp
@@ -42,28 +42,18 @@ class Solution
 public:
   int findContentChildren(vector<int> &g, vector<int> &s)
   {
-    int cookiesNums = s.size();
-    if (cookiesNums == 0)
-      return 0;
     sort(g.begin(), g.end());
     sort(s.begin(), s.end());
 
-    int maxNum = 0;
-    int cookieIndex = cookiesNums - 1;
-    int childIndex = g.size() - 1;
-    while (cookieIndex >= 0 && childIndex >= 0)
+    int j = 0;
+    for (int i = 0; i < s.size() && j < g.size(); i++)
     {
-      if (s[cookieIndex] >= g[childIndex])
+      if (s[i] >= g[j])
       {
-        maxNum++;
-        cookieIndex--;
-        childIndex--;
-      }
-      else
-      {
-        childIndex--;
+        j++;
       }
     }
-    return maxNum;
+
+    return j;
   }
 };
