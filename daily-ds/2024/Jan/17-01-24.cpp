8be67fa9f4457eef7a65936286f8411ce79d2493
Paritosh Batish <batishparitosh2@gmail.com>
2024-01-17 18:12:24 +0530

    dds 17-01-24

---
+++ daily-ds/2024/Jan/17-01-24.cpp
@@ -0,0 +1,62 @@
+/*
+
+URL: https://leetcode.com/problems/unique-number-of-occurrences/description/
+
+1207. Unique Number of Occurrences
+
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+
+ 
+Constraints:
+
+  1 <= arr.length <= 1000
+  -1000 <= arr[i] <= 1000
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool uniqueOccurrences(vector<int> &arr)
+  {
+    unordered_map<int, int> mpp;
+    unordered_set<int> st;
+
+    for (int ele : arr)
+    {
+      mpp[ele]++;
+    }
+
+    for (auto it : mpp)
+    {
+      if (st.find(it.second) != st.end())
+      {
+        return false;
+      }
+      st.insert(it.second);
+    }
+
+    return true;
+  }
+};
