9f6c501578c81e05d696a3a5a011cf8eb222d96d
Paritosh Batish <batishparitosh2@gmail.com>
2024-03-08 15:45:31 +0530

    dds 08-03-24

---
+++ daily-ds/2024/03 Mar/08-03-24.cpp
@@ -0,0 +1,58 @@
+/*
+
+URL: https://leetcode.com/problems/count-elements-with-maximum-frequency/description/
+
+3005. Count Elements With Maximum Frequency
+
+You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the array.
+
+ 
+Example 1:
+
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+
+ 
+Constraints:
+
+	1 <= nums.length <= 100
+	1 <= nums[i] <= 100
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        int maxi = 0;
+
+        for (int x : nums) {
+            mpp[x]++;
+            maxi = max(maxi, mpp[x]);
+        }
+
+        int num = 0;
+        for (auto it : mpp) {
+            if (it.second == maxi) num++;
+        }
+
+        return num * maxi;
+    }
+};
